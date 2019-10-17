9f3b2685214b0b341bb2a5bca03c4f64c0db067a
Sena543 <42738493+Sena543@users.noreply.github.com>
2019-10-17 02:50:15 +0000

    Updated Files

--- app.js
+++ app.js
@@ -1,46 +1,50 @@
-const express = require('express')
-const parser = require('body-parser')
-const mongoose = require('mongoose')
-const session = require('express-session')
-const passport = require('passport')
-const ejs =require('ejs')
-const bcrypt = require('bcrypt')
-const path = require('path')
-const registered_students = require('./models/studentModel.js')
-const router = require('./routes/routes.js');
+const express = require('express');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const passport = require('passport');
+const ejs = require('ejs')
+const bcrypt = require('bcryptjs');
+//const students = ;
+
+const path = require('path');
+
+const app = express();
+
+require('./config/passport')(passport);
 
 let db = mongoose.connection;
-mongoose.connect('mongodb://localhost/student_database', {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect('mongodb://localhost/student_database', {useNewUrlParser: true, useUnifiedTopology: true});
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-    console.log('Database Connected')
+    console.log('Database Connected');
 });
 
-const app = express()
-require('./config/passport.js')(passport)
+app.use(express.urlencoded({extended: true }));
+app.use(require('cookie-parser')());
+app.use(express.json())
+
+app.use(require('express-ejs-layouts'));
+app.use(express.static(path.join(__dirname + '/views')))
+app.set('views', 'views');
+app.set('view engine', 'ejs');
 
 
-app.set('views', path.join(__dirname, 'views'));
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
-
-
-//session to save login session
 app.use(session({
-    secret: 'secret',
+    secret: 'secret-student',
     resave: true,
     saveUninitialized: true,
+   // cookie: { secure: true }
   }))
-//passport middleware
+
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(parser.urlencoded({extended: true }));
-app.use(express.json())
-app.use(router);
-app.use(registered_students);
+app.use( require('./routes/routes'));
+app.use(require('./models/hallModel.js'));
+app.use(require('./models/studentModel.js'));
 
-const port = process.env.port || 5000;
+
+const port = process.env.PORT || 8000;
 app.listen(port,()=>{
-    console.log(`Served on ${port}`)
+    console.log(`Server running on localhost:${port} `);
 })
